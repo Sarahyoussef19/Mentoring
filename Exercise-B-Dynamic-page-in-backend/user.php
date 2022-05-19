1f6ed5f9b4a6779dd572d75f3469fec15e0b6398
sarah <sarahdevmail@gmail.com>
2022-05-19 12:54:30 +0200

    Add database to the project

--- Exercise-B-Dynamic-page-in-backend/user.php
+++ Exercise-B-Dynamic-page-in-backend/user.php
@@ -1,15 +1,39 @@
+<!-- URL paramater to display what is shown in the url id -->
+
 <?php
-$userInfos = [
-    'id' => 1,
-    'forename' => 'sarah',
-    'lastname' => 'Youssef',
-    'age' => 32,
-    'adress' => 'Entença,95'
-    // etc
-];
-// $@% syntax error
-// echo php_ini_loaded_file();
-?> 
+echo $_GET['id'];
+var_dump($_GET)
+?>
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$userId = $_GET['id'];
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, 'mentoring');
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+// echo "Connected successfully";
+
+$query = "SELECT * FROM user WHERE id=$userId"; // WHERE user_id = " . $user_id;
+echo $query;
+$result = mysqli_query($conn, $query);
+//$row   = mysqli_fetch_row($result);
+$userInfos = $result->fetch_assoc();
+// echo '<pre>';
+// var_dump ($userInfos);
+
+$conn->close();
+?>
+
+<?php
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -18,8 +42,7 @@ $userInfos = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Exercise A</title>
 </head>
 
@@ -28,11 +51,21 @@ $userInfos = [
         <h1>Exercise A</h1>
         <div class="card">
             <div class="card-body">
-                Id: <?php echo $userInfos['id'] ?><br>
-                Forename: <?php echo $userInfos['forename'] ?><br>
-                Lastname: <?php echo $userInfos['lastname'] ?><br>
-                Age: <?php echo $userInfos['age'] ?> <br>
-                Adress: <?php echo $userInfos['adress'] ?>
+                <?php
+                if ($userInfos == null) {
+                ?>
+                    sorry no user found
+                <?php
+                } else {
+                ?>
+                    Id: <?php echo $userInfos['id'] ?> <br>
+                    Forename: <?php echo $userInfos['forename'] ?><br>
+                    Lastname: <?php echo $userInfos['lastname'] ?><br>
+                    Age: <?php echo $userInfos['age'] ?> <br>
+                    Adress: <?php echo $userInfos['adress'] ?>
+                <?php
+                }
+                ?>
             </div>
         </div>
     </div>
